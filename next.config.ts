80246a46db1243180e5b27cd91367cf6a5904d34
Tom <70413548+TomFrda@users.noreply.github.com>
2025-02-09 23:46:44 +0100

    [FIX] githubpage

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'dist',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  basePath: '/myportfolio',
-  assetPrefix: '/myportfolio',
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
